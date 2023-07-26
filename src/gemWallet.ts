a62566a8b727d98c468807139b5f24367408900b
garantor <sundayafolabi992@gmail.com>
2023-07-26 02:44:10 +0100

    refactor(Stable version 0.1.20): :zap: refactor code for npm typing
    refactor code for npm typing and also fix some known bugs

--- src/gemWallet.ts
+++ src/gemWallet.ts
@@ -1,38 +1,48 @@
-import { isInstalled, getAddress, getNetwork, signMessage } from '@gemwallet/api'
+import {
+  isInstalled,
+  getAddress,
+  getNetwork,
+  sendPayment,
+  IsInstalledResponse,
+  GetAddressResponse,
+  GetNetworkResponse,
+} from "@gemwallet/api";
 
-
+import { XrplClient } from "xrpl-client";
 
 export interface WalletInitResponse {
-    isInstalled: boolean,
-    publicKey?: string,
-    currentNetwork?: any
+  isInstalled: boolean;
+  publicKey?: string;
+  currentNetwork?: any;
 }
 
+export async function gemWalletInit(): Promise<{
+  isInstalled: () => Promise<IsInstalledResponse>;
+  publicKey: GetAddressResponse;
+  currentNetwork: GetNetworkResponse;
+}> {
+  let checkInstall = await isInstalled();
+  if (checkInstall) {
+    let address = await getAddress();
+    let network = await getNetwork();
 
-export async function gemWalletInit() {
+    let response = {
+      isInstalled: isInstalled,
+      publicKey: address,
+      currentNetwork: network, //this is the current network the wallet is connected to
+    };
+    return response;
+  }
 
-    let checkInstall = await isInstalled()
-    if (checkInstall) {
-        let address = await getAddress()
-        let network = await getNetwork()
-
-        let response = {
-            isInstalled: isInstalled,
-            publicKey: address,
-            currentNetwork: network //this is the current network the wallet is connected to
-        }
-        return response
-
-    }
-
-    throw new Error("Gem Wallet not install")
-
+  throw new Error("Gem Wallet not install");
 }
 
+export async function signGemTransaction(transaction: any, client: XrplClient) {
+  let signedTx = await sendPayment(transaction);
 
-export async function signGemTransaction(transaction:any, client:any) {
-    let signedTx =  await signMessage(transaction)
-    let submittx = client.submitAndWait(signedTx.result?.signedMessage!)
-    console.log(submittx)
-    return submittx
+  console.log(signedTx);
+  //   let submittx = await client.send({ command: "submit", tx_blob: signedTx });
+  //   client.close();
+  //   console.log(submittx);
+  return signedTx;
 }
